cbb44edafecc97ab0e972178caff26844401351f
mochwahyu199 <wahyuzuhri28@gmail.com>
2021-05-03 21:29:38 +0700

    Update config.php
    setting encryption_key session - ben gak podo aplikasi laine

--- application/config/config.php
+++ application/config/config.php
@@ -326,7 +326,7 @@ $config['cache_query_string'] = FALSE;
 | https://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = 'abcde~!@#$%^&*()_+';
+$config['encryption_key'] = 'e-surat#abcde~!@#$%^&*()_+';
 
 /*
 |--------------------------------------------------------------------------
