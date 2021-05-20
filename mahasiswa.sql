3e72a054f78b1ade6e970378ccd2a7bb4d25346e
mochwahyu199 <wahyuzuhri28@gmail.com>
2021-05-20 15:01:50 +0700

    update biasa

--- mahasiswa.sql
+++ mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2021 at 09:12 AM
+-- Generation Time: May 20, 2021 at 10:00 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.2.34
 
